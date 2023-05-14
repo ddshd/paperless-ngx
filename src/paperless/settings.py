674901340c920f2aab77e062806da4caa6516c46
Dhrumil Shah <ddshd@users.noreply.github.com>
2023-05-14 03:28:00 -0400

    Added PAPERLESS_X_FRAME_OPTIONS env support
    Added PAPERLESS_X_FRAME_OPTIONS env support to allow users to specify their own values.

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -399,7 +399,7 @@ if ENABLE_HTTP_REMOTE_USER:
     )
 
 # X-Frame options for embedded PDF display:
-X_FRAME_OPTIONS = "ANY" if DEBUG else "SAMEORIGIN"
+X_FRAME_OPTIONS = "ANY" if DEBUG else os.getenv("PAPERLESS_X_FRAME_OPTIONS", "SAMEORIGIN")
 
 
 # The next 3 settings can also be set using just PAPERLESS_URL
